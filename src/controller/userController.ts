ee78b0484590a57fec6c135366289fb7c3a4025a
hanlee <me@hanlee.co>
2020-12-23 16:00:49 +0900

    chore: move to controllers

--- src/controller/userController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-
-const showMyStudentCard=async(req: Request,res: Response,next: NextFunction)=>{
-
-}
-
-export{
-    showMyStudentCard,
-}
