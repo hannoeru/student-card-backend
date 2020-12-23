2e1c7db065606dd05e95b7a0d079f63381f6d850
かえる <kasuo.takumi@gmail.com>
2020-12-23 15:28:34 +0900

    Fix env読み込み

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
 import express from 'express'
 import morgan from 'morgan'
-import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { installPlugins } from '@/plugins'
 import { registerRoutes } from '@/routes'
 import { passport } from '@/passport'
 import { errorHandler } from '@/middleware/errorHandler'
-dotenv.config()
 
 const PORT = process.env.PORT || 8000
 
