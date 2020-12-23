0cad1c377083043398c5133c7e1a01e3fdb280b4
OGA <OGA45@users.noreply.github.com>
2020-12-23 15:54:07 +0900

    chore: init

---
+++ src/controller/authController.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express'
+
+const userLogin=async(req: Request,res: Response,next: NextFunction)=>{
+
+}
+const createNewAccount=async(req: Request,res: Response,next: NextFunction)=>{
+
+}
+
+export{
+    userLogin,
+    createNewAccount,
+}
---
+++ src/controller/userController.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+const showMyStudentCard=async(req: Request,res: Response,next: NextFunction)=>{
+
+}
+
+export{
+    showMyStudentCard,
+}
