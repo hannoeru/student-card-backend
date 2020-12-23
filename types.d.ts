8b6c0a7c973360d28fc6e76fa4743723c3825680
hanlee <me@hanlee.co>
2020-12-23 17:07:43 +0900

    fix: type

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     // Additional environment variables
     HASH_KEY: string
-    NEXT_PUBLIC_APP_NAME: string
-    NEXT_PUBLIC_APP_URL: string
+    PUBLIC_URL: string
+    PORT: string
   }
 }
