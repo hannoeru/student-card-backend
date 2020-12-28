ca180a691c1e63d71a04c849b9133642219887fe
hanlee <me@hanlee.co>
2020-12-29 01:11:21 +0900

    chore: log error in development

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,10 @@
 import { ErrorRequestHandler } from 'express'
 import { ErrorResponse } from '@/lib'
+
+const isProd = process.env.NODE_ENV === 'production'
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (!isProd) console.log(err)
+
   if (err instanceof ErrorResponse) {
     res
       .status(err.statusCode)
