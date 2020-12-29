288fef93547dbc12ffdc8fc639bf5766161fb203
hanlee <me@hanlee.co>
2020-12-29 22:38:07 +0900

    feat: upload image

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import authRouter from './auth'
 import userRouter from './user'
+import uploadRouter from './upload'
 
 export function registerRoutes(app: Express) {
   app.use('/api/v1/auth', authRouter)
   app.use('/api/v1/user', userRouter)
+  app.use('/api/v1/upload', uploadRouter)
 }
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -5,7 +5,7 @@ import { uploadImage, getImage } from '@/controllers/uploadController'
 import { requireAuth } from '@/guards/requireAuth'
 
 const router = Router()
-const upload = multer()
+const upload = multer({ storage: multer.memoryStorage() })
 
 router.post('/image', requireAuth, upload.single('image'), uploadImage)
 router.get('/image/:id', requireAuth, getImage)
